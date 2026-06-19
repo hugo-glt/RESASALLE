c83f896db7ec3d3480c82b5c6e79b539c45ddabe
hugo-glt <hg.gilet@gmail.com>
2026-06-19 18:26:42 +0200

    feat: HomePage modification feed

--- frontend/src/views/Users/HomePage.vue
+++ frontend/src/views/Users/HomePage.vue
@@ -10,25 +10,32 @@ function goSearch() {
 </script>
 
 <template>
-    <div class="flex items-start justify-center gap-10 p-8 min-h-screen">
-    <section>
-        <div class="flex items-center gap-4">
-            <h2 class="text-2xl font-bold mb-4">BOOKED</h2>
-            <h2 class="text-2xl font-bold mb-4">ROOMS</h2>
+    <div class="flex items-start justify-center gap-50 p-8 min-h-screen">
+        <div class="flex items-start justify-center p-8 min-h-screen">
+            <section class="flex flex-col items-center min-h-100">
+                <div class="flex items-center gap-4">
+                    <h2 class="text-2xl font-bold mb-4">BOOKED</h2>
+                    <h2 class="text-2xl font-bold mb-4">ROOMS</h2>
+                </div>
+                <div>
+                <button @click="goSearch" class="flex flex-col items-center cursor-pointer mt-auto">
+                    <img src="../../assets/icons/plus-large-svgrepo-com.svg" class="w-10 h-10" />
+                    <span>BOOK A ROOM</span>
+                </button>
+                </div>
+            </section>
         </div>
-        <!-- Boutons ADD -->
-        <div class="flex items-center gap-4 cursor-pointer">
-            <button @click="goSearch" >
-                <img src="../../assets/icons/plus-large-svgrepo-com.svg" class="w-10 h-10" />
-                <span class="ml-2">BOOK A ROOM</span>
-            </button>
-        </div>
-    </section>
-    <section>
-        <div class="w-75 h-100 bg-gray-300 relative">
-            <img src="../../assets/icons/funnel-sort-svgrepo-com.svg" class="w-5 h-5" />
-            <div class="w-60 h-1 bg-gray-500"></div>
-        </div>
-    </section>
+        <section class="w-125 h-125 bg-gray-300 relative">
+            <div class="flex justify-center gap-20 mt-2">
+                <img src="../../assets/icons/arrow-left-svgrepo-com.svg"  class="w-5 h-5">
+                <img src="../../assets/icons/arrow-right-svgrepo-com.svg" class="w-5 h-5">
+                <div class="flex flex-col items-start">
+                    <img src="../../assets/icons/funnel-sort-svgrepo-com.svg" class="w-5 h-5 right-5" />
+                </div>
+            </div>
+            <div class="flex justify-center" >
+                <div class="w-110 h-1 bg-gray-500"></div>
+            </div>
+        </section>
     </div>
 </template>
